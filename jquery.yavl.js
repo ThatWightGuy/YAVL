5b15fd1a6d3f91abaa1fa560a95504e71baa4649
traviswight1 <tman.wight@gmail.com>
2020-12-23 16:16:23 -0800

    Basic Validation Working

--- jquery.yavl.js
+++ jquery.yavl.js
@@ -1,51 +1,93 @@
 (function ($) {
+	// Pre-defined options for Label Placements (labelTypes):
+	const labelFunctions = {
+		'top': function(container, field) {
+			container.addClass('y-label-top');
+			field.appendTo(container);
+			return true;
+		},
+		'bottom': function(container, field) {
+			container.addClass('y-label-bottom');
+			return true;
+		},
+		'left': function(container, field) {
+			container.addClass('y-label-left');
+			field.appendTo(container);
+			return true;
+		},
+		'right': function(container, field) {
+			container.addClass('y-label-right');
+			return true;
+		},
+		'infield': function(container, field) {
+			container.addClass('y-label-infield');	
+			return true;
+		},
+		'infield-float': function(container, field) {
+			container.addClass('y-label-float');
+			return true;
+		}
+	}
+
+	// Pre-defined options for yval attribute. Custom attributes are created
+	// in "validOptions" parameter in the options object on init.
+	var validation = {
+		'email': /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/,
+		'phone': /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im,
+	}
+
+	var addValid = function(input) {
+		input.parent().addClass('yavl-valid');
+		input.parent().removeClass('yavl-invalid');
+	}
+
+	var addInvalid = function(input) {
+		input.parent().addClass('yavl-invalid');
+		input.parent().removeClass('yavl-valid');
+	}
+
+	var validateInput = function(){
+		$('[yavl]').on('input', function() {
+			if (this.value) {
+				$(this).parent().addClass('yavl-filled');
+			}
+			else {
+				$(this).parent().removeClass('yavl-filled');
+			}
+
+			if (validation[$(this).attr('yavl')]) {
+				if ($(this).attr('yavl-req') != 'false') {
+					if (validation[$(this).attr('yavl')].test(this.value)) {
+						addValid($(this));
+					}
+					else {
+						addInvalid($(this));
+					}
+				}
+				else {
+					addValid($(this));
+				}
+			}
+			else {
+				if ($(this).attr('yavl-req') != 'false') {
+					if (this.value) addValid($(this));
+					else addInvalid($(this));
+				}
+				else addValid($(this));
+			}
+		});
+	}
 
 	var labelClass = function (options, container, field) {
 		if (options && options.labelType) {
-			switch (options.labelType) {
-				case 'top':
-					container.addClass('y-label-top');
-					field.appendTo(container);
-
-					break;
-				case 'bottom':
-					container.addClass('y-label-bottom');
-
-					break;
-				case 'left':
-					container.addClass('y-label-left');
-					field.appendTo(container);
-
-					break;
-				case 'right':
-					container.addClass('y-label-right');
-
-					break;
-				case 'infield':
-					container.addClass('y-label-infield');	
-
-					break;
-				case 'infield-float':
-					container.addClass('y-label-float');
-
-					break;
-				default: 
-					container.addClass(options.labelType);
-
-					break;
+			if (!labelFunctions[options.labelType](container, field)) {
+				container.addClass(options.labelType);
 			}
 		}
 	}
 
-	var validateInput = function(options){
-		$('[yavl]').on('input', function() {
-			// console.log(this.tagName);
-			console.log(); 
-		});
-	}
-
-	$.fn.yavl = function(options) {
-		const fields = this.find('[yavl]');
+	var fieldInit = function (par, options) {
+		const fields = par.find('[yavl]');
 
 		for (var i = 0; i < fields.length; i++) {
 			var parent = $(fields[i]).parent();
@@ -75,8 +117,16 @@
 				
 				labelClass(options, container, $(fields[i]));
 			}
-
-			validateInput(options);
 		}
+	}
+
+	$.fn.yavl = function (options) {
+		// Merge custom validation from options into validation object:
+		if (options.validation) {
+			validation = Object.assign(validation, options.validation);
+		}
+
+		fieldInit(this, options);
+		validateInput(options);
 	}
 }(jQuery));
